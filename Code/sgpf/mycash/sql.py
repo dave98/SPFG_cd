583181dcf59c3b8aca93835cad5e9c8e31d4f3d8
Percy Maldonado Quispe <percy.maldonado@ucsp.edu.pe>
2018-06-06 20:13:58 -0500

    verify_category and create_category

--- Code/sgpf/mycash/sql.py
+++ Code/sgpf/mycash/sql.py
@@ -6,29 +6,32 @@ from django.db import connection
     that were performed.
 """
 
+
 class DB:
     def __init__(self):
         self.db = 'mycash'
 
     # The sum of the income per day in the last days
     @staticmethod
-    def income_amount(idu, day):
+    def income_day(idu, day):
         with connection.cursor() as cursor:
             cursor.execute("select * from income_day(%s,%s)", [idu, day])
             return cursor.fetchall()
 
     # The sum of the expense per day in the last days
     @staticmethod
-    def expense_amount(idu, day):
+    def expense_day(idu, day):
         with connection.cursor() as cursor:
             cursor.execute("select * from expense_day(%s,%s)", [idu, day])
             return cursor.fetchall()
 
-    # Verify the login, checking the existence of the user
-    # returns 0 if it does not exist, and if it exists returns
-    # the user's id.
     @staticmethod
-    def validate_user(email, password):
+    def create_category(name, id_us):
         with connection.cursor() as cursor:
-            cursor.execute("select * from validate(%s,%s)", [email, password])
-            return int(cursor.fetchone()[0])
+            cursor.execute("select * from create_category(%s,%s)", [name, id_us])
+
+    @staticmethod
+    def verify_category(name, id_us):
+        with connection.cursor() as cursor:
+            cursor.execute("select * from verify_category(%s,%s)", [name, id_us])
+            return cursor.fetchall()[0][0]
